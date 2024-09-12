25c4916b83d5e222730f81c21fa4771f22ccb6fc
alexandros3015 <105521378+alexandros3015@users.noreply.github.com>
2024-09-12 09:47:24 -0400

    comments and clarification

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 # point-intercept-thingy
 
-I *REALLY* didn't feel like doing ixl code 2R9 normally so I make this thingy
+I *REALLY* didn't feel like doing ixl code 2R9 normally so I make this thingy. PLEASE NOTE: Whichever X value is highest is going to be the ordered pair for x and y_2. Ex. (-7, 0) and (7, -2). 7 > -7 therefore x_2 is 7 and y_2 is -2. 
--- main.c
+++ main.c
@@ -1,8 +1,10 @@
 #include <stdio.h>
 
 int main() {
+    // All values needed
     double x_1, y_1, y_2, x_2 = 0;
 
+    // Get all needed values
     printf("y_2: ");
     scanf("%lf", &y_2);
 
@@ -12,13 +14,14 @@ int main() {
     printf("\nx_2: ");
     scanf("%lf", &x_2);
 
-    printf("x_1: \n");
+    printf("x_1: ");
     scanf("%lf", &x_1);
 
+    // Get the top of the fraction, the bottom of the fraction, and the total
     double top, bottom = 0;
     
     top = y_2-y_1;
-    printf("Top of the fraction: %lf\n", top);
+    printf("\nTop of the fraction: %lf\n", top);
     
     bottom = x_2-x_1;
     printf("Bottom of the fraction: %lf\n", bottom);
@@ -26,8 +29,10 @@ int main() {
     printf("Answer: %lf / %lf\n", top, bottom);
     
     double total = top/bottom;
-    double yintercept = y_1 - (x_1 * total);
+    double yintercept = y_1 - (x_1 * total); // Gets y-intercept
     
     printf("Possible simplified fraction: %lf", total);
     printf("\nThe y-intercept is %lf", yintercept);
+    
+    return 0;
 }
